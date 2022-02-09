cf12f7d4c29e2e8a4cb5dec9d26a5fbc38445f9b
Brian Simpson <bsimps3@gmail.com>
2022-02-09 12:37:09 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let list: number[] = []
+for (let index = 0; index < 50; index++) {
+    list.push(randint(0, 100))
+}
+let counter = 0
+for (let index = 0; index <= list.length - 1; index++) {
+	
+}
+for (let value of list) {
+    console.log(value)
+}
