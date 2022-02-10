611e09ffbdf2107681c7f28a7998ebdaac03034b
Brian Simpson <bsimps3@gmail.com>
2022-02-10 12:15:12 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
+let temp = 0
 let list: number[] = []
+let counter = 10
 for (let index = 0; index < 50; index++) {
     list.push(randint(0, 100))
 }
-let counter = 0
-for (let index = 0; index <= list.length - 1; index++) {
-	
+while (counter > 0) {
+    counter = 0
+    for (let index = 0; index <= list.length - 2; index++) {
+        if (list[index] > list[index + 1]) {
+            temp = list[index]
+            list[index] = list[index + 1]
+            list[index + 1] = temp
+            counter += 1
+        }
+    }
 }
-for (let value of list) {
-    console.log(value)
-}
+console.log(list)
